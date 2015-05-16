7280686876b37aaf51c8842a4aa9986da8136dad
syedshahwar <syed.shahwar7@gmail.com>
2015-05-16 12:19:51 +0500

    Qazi Crying

---
+++ src/main/java/DAO/BooksDAO.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+
+import static POJOS.BDHibernateUtil.getSessionFactory;
+import POJOS.Books;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
+
+
+public class BooksDAO {
+    
+    Session session = null;
+    Transaction tx = null;
+    
+    public BooksDAO()
+    {
+         Session session = getSessionFactory().openSession();
+    }
+    
+   
+    public void AddBook(Books Bk){
+  
+        tx=session.beginTransaction();
+        session.persist(Bk);
+        tx.commit();
+    }
+        
+    public List<Books> FindAllBooks()
+    {
+        Query q=session.createQuery("from Books");  
+        List<Books> list=q.list();  
+
+        return list;  
+    }
+    
+    public List<Books> FindBooksByName(String BookName)
+    {
+        Query q=session.createQuery("from Books where bookName=: name ");  
+        q.setParameter("name",BookName);
+        List<Books> list=q.list();  
+
+        return list;  
+    }
+    
+    public List<Books> FindBooksByGrade(String Grade)
+    {
+        Query q=session.createQuery("from Books where class_=:grade "); 
+        q.setParameter("grade",Grade);
+        List<Books> list=q.list();  
+
+        return list;  
+    }
+    
+    public List<Books> FindBooksByAuthor(String Author)
+    {
+        Query q=session.createQuery("from Books where author=:author "); 
+        q.setParameter("author",Author);
+        List<Books> list=q.list();  
+
+        return list;  
+    }
+     
+    
+    public void DeleteBook(String BookName)
+    {
+        tx=session.beginTransaction();
+        Query q = session.createQuery("delete from Books where bookName=: name");
+        q.setParameter("name", BookName);
+        q.executeUpdate();
+        tx.commit();
+    }
+    
+    public void UpdateBook(String BookName,String Description )
+    {
+        tx=session.beginTransaction();
+        Query q=session.createQuery("Update Boooks set bookDescription=:desc where bookName=:name");
+        q.setParameter("name",BookName);
+        q.setParameter("desc",Description);
+        q.executeUpdate();
+        tx.commit();
+    }
+        
+        
+    public void CloseSession(){
+        session.close();
+    }        
+ }
+    
+
--- src/main/java/DAO/UsersDAO.java
+++ src/main/java/DAO/UsersDAO.java
@@ -5,10 +5,95 @@
  */
 package DAO;
 
-/**
- *
- * @author Farjad
- */
+
+import static POJOS.BDHibernateUtil.getSessionFactory;
+import POJOS.User;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class UsersDAO {
+    
+    Session session = null;
+    Transaction tx = null;
+    
+    public UsersDAO()
+    {
+         Session session = getSessionFactory().openSession();;
+    }
+    
+   
+    public void AddUser(User Us){
+            
+        Transaction tx=session.beginTransaction();  
+        session.persist(Us);
+        tx.commit();
+    }
+        
+    public List<User> FindUsers(String Email, String Password)
+    {
+        Query q=session.createQuery("from User where email=:mail and password=:pass"); 
+        q.setParameter("mail",Email);
+        q.setParameter("pass",Password);
+        List<User> list=q.list();  
+
+        return list;  
+    }
+     
+    public void DeleteUser(String Email)
+    {
+        tx=session.beginTransaction();
+        Query q = session.createQuery("delete from User where email= :mail");
+        q.setParameter("mail", Email);
+        q.executeUpdate();
+        tx.commit();
+    }
+    
+    public void UpdateUserPass(String Email, String Password )
+    {
+        tx=session.beginTransaction();
+        Query q=session.createQuery("Update User set password=:pass where email=:mail");
+        q.setParameter("mail",Email);
+        q.setParameter("pass",Password);
+        q.executeUpdate();
+        tx.commit();
+    }
+    
+    public void UpdateUserAddress(String Email, String Address )
+    {
+        tx=session.beginTransaction();
+        Query q=session.createQuery("Update User set address=:add where email=:mail");
+        q.setParameter("mail",Email);
+        q.setParameter("add",Address);
+        q.executeUpdate();
+        tx.commit();
+    }
+    
+    public void UpdateUserCity(String Email, String City )
+    {
+        tx=session.beginTransaction();
+        Query q=session.createQuery("Update User set city=:city where email=:mail");
+        q.setParameter("mail",Email);
+        q.setParameter("city",City);
+        q.executeUpdate();
+        tx.commit();
+    }
+    
+    public void UpdateUserCountry(String Email, String Country )
+    {
+        tx=session.beginTransaction();
+        Query q=session.createQuery("Update User set country=:country where email=:mail");
+        q.setParameter("mail",Email);
+        q.setParameter("country",Country);
+        q.executeUpdate();
+        tx.commit();
+    }
+        
+        
+    public void CloseSession(){
+        session.close();
+    }
+
     
 }
